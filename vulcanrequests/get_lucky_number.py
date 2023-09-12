d441618da0aebc34dd2ff4e0b24ea539e6c4fd30
OhFoxies <w.rejner@gmail.com>
2023-09-12 22:16:11 +0200

    Typehints

--- vulcanrequests/get_lucky_number.py
+++ vulcanrequests/get_lucky_number.py
@@ -1,11 +1,13 @@
+import vulcan.data
 from vulcan import Vulcan, Keystore, Account
+from typing import Any
 
 
 async def get_lucky_number(keystore, account) -> int:
-    keystore_ = Keystore.load(keystore)
-    account_ = Account.load(account)
-    user = Vulcan(account=account_, keystore=keystore_)
+    keystore_: Any = Keystore.load(keystore)
+    account_: Any = Account.load(account)
+    user: Vulcan = Vulcan(account=account_, keystore=keystore_)
     await user.select_student()
-    lucky_number = await user.data.get_lucky_number()
+    lucky_number: vulcan.data.LuckyNumber = await user.data.get_lucky_number()
     await user.close()
     return lucky_number.number
