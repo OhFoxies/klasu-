047f74f978e25bc876f2dafeaa4e16f244577361
OhFoxies <w.rejner@gmail.com>
2023-09-12 23:02:22 +0200

    Cosmetic changes

--- commands/CommandDeleteSchool.py
+++ commands/CommandDeleteSchool.py
@@ -10,7 +10,9 @@ class DeleteSchool(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    @discord.slash_command(description="Usuwa wybraną szkołę", name="usun-szkole", dm_permission=False,
+    @discord.slash_command(description="Usuwa wybraną szkołę",
+                           name="usun-szkole",
+                           dm_permission=False,
                            force_global=True,
                            default_member_permissions=discord.Permissions(permissions=8))
     async def delete_school(self, interaction: discord.Interaction,
